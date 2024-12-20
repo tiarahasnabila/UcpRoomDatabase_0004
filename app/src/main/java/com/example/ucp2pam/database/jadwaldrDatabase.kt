a35fce56f70077685b4250cce4b9d83d3a20ef2c
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-20 12:40:28 +0700

    membuat properti dari kelas jadwaldrDatabase

--- app/src/main/java/com/example/ucp2pam/database/jadwaldrDatabase.kt
+++ app/src/main/java/com/example/ucp2pam/database/jadwaldrDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2pam.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ucp2pam.dao.DokterDao
 import com.example.ucp2pam.dao.JadwalDao
@@ -12,4 +14,20 @@ abstract class jadwaldrDatabase : RoomDatabase() {
     //mendefinisikan fungsi mengakses data dokter dan jadwal
     abstract fun dokterDao(): DokterDao
     abstract fun jadwalDao(): JadwalDao
+
+    companion object{
+        @Volatile // memastikan bahwa nilai variabel instance selalu sama di semua
+        private var Instance: jadwaldrDatabase?= null
+
+        fun getDatabase(context: Context): jadwaldrDatabase{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    jadwaldrDatabase::class.java, //class database
+                    "RsDatabase" // Nama database
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
 }
