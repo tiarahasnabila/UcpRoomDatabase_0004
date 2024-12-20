26b68910e5598eaf063e38ed5e8c68dc63eac321
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-20 12:17:17 +0700

    menambahkan,mengambil,dan mendapatkan data baru jadwal

--- app/src/main/java/com/example/ucp2pam/dao/JadwalDao.kt
+++ app/src/main/java/com/example/ucp2pam/dao/JadwalDao.kt
@@ -10,14 +10,18 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface JadwalDao {
+
     @Insert
     suspend fun insertJadwal(jadwal: Jadwal)
 
-    @Query("SELECT * FROM jadwal ORDER BY namaDokter ASC")
-    //Untuk membungkus data
+    //getAllJadwal
+    @Query("SELECT * FROM jadwal ORDER BY namaPasien ASC")
     fun getAllJadwal(): Flow<List<Jadwal>>
 
-    //Query untuk mendapat data berdasarkan
+    //Query getJadwal
+    @Query("SELECT * FROM jadwal WHERE idJadwal = :idJadwal")
+    fun getJadwal(idJadwal: String): Flow<Jadwal>
+
 
 
 }
