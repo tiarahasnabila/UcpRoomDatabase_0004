c18ed84efa9996113bbe6bbcbc84699f77f51c15
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-20 12:05:40 +0700

    Menambahkan DAO untuk entitas Dokter, serta menambahkan dan mengambil semua data dokter

--- app/src/main/java/com/example/ucp2pam/dao/DokterDao.kt
+++ app/src/main/java/com/example/ucp2pam/dao/DokterDao.kt
@@ -2,27 +2,23 @@ package com.example.ucp2pam.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.ucp2pam.entity.Dokter
 import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface DokterDao {
+
     @Insert
     suspend fun insertDokter(dokter: Dokter)
-    //Query untuk mendapat seluruh data mahasiswa yang diurutkan dari A-Z, A = Ascending
-    //Banyak data menggunakan List
-    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
-    //Untuk membungkus data
-    fun getAllDokter(): Flow<List<Mahasiswa>>
 
-    //Query untuk mendapat data berdasarkan nim mahasiswa
-    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
-    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
+    //getAllDokter
+    @Query("SELECT * FROM dokter ORDER BY namaDokter ASC")
 
-    @Delete
-    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+    fun getAllDokter(): Flow<List<Dokter>>
 
-    @Update
-    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+    //getDokter
+    @Query("SELECT * FROM dokter WHERE id = :id")
+    fun getDokter(id: String) : Flow<Dokter>
 }
