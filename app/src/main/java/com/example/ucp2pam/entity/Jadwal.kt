2e8b420cf6794247ff51e6aedf503dd3270818da
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-20 13:25:25 +0700

    memperbarui id jadwal

--- app/src/main/java/com/example/ucp2pam/entity/Jadwal.kt
+++ app/src/main/java/com/example/ucp2pam/entity/Jadwal.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "jadwal")
 data class Jadwal(
     @PrimaryKey
-    val id:String,
+    val idJadwal:String,
     val namaDokter: String,
     val namaPasien: String,
     val noHp: String,
