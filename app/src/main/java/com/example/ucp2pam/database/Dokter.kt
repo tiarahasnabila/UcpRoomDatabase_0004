294f1345bd82625aec589252de272fd9db84fcc1
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-23 11:56:05 +0700

    mengupdate file dokter dan jadwal

---
+++ app/src/main/java/com/example/ucp2pam/database/Dokter.kt
@@ -0,0 +1,26 @@
+package com.example.ucp2pam.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dokter")
+data class Dokter(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val namaDokter: String,
+    val spesialis: String,
+    val klinik: String,
+    val noHp: String,
+    val jamKerja: String,
+)
+
+@Entity(tableName = "jadwal")
+data class Jadwal(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val namaDokter: String,
+    val namaPasien: String,
+    val noHp: String,
+    val tanggalKonsultasi: String,
+    val status: String,
+)
+
+
