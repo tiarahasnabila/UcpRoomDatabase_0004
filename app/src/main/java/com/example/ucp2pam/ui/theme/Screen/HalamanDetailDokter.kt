0549077161969fc0d7c2d2ee344f9c926278a886
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-24 11:52:49 +0700

    menambahkan card untuk menampilkan data dokter

--- app/src/main/java/com/example/ucp2pam/ui/theme/Screen/HalamanDetailDokter.kt
+++ app/src/main/java/com/example/ucp2pam/ui/theme/Screen/HalamanDetailDokter.kt
@@ -140,9 +140,20 @@ fun DokterCard(dokter: Dokter, navController: NavController, onDelete: () -> Uni
         "gizi klinik" -> Color.Cyan
         else -> Color.Gray
     }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        shape = RoundedCornerShape(30.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        )
 
 
 
 
 
-}
+    }
