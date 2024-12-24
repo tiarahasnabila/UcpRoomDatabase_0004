60cfe086e10d9d6671d7c3a766f55159e227a199
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-24 12:12:36 +0700

    composable function yang digunakan untuk menampilkan dan mengelola informasi dokter

---
+++ app/src/main/java/com/example/ucp2pam/ui/theme/Screen/HalamanDokter.kt
@@ -0,0 +1,29 @@
+package com.example.ucp2pam.ui.theme.Screen
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.ucp2pam.ViewModel.MainViewModel
+import com.example.ucp2pam.database.Dokter
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanDokter(viewModel: MainViewModel, navController: NavController) {
+    val dokterList by viewModel.dokterList.observeAsState(emptyList())
+    var nama by remember { mutableStateOf("") }
+    var spesialis by remember { mutableStateOf("") }
+    var klinik by remember { mutableStateOf("") }
+    var noHp by remember { mutableStateOf("") }
+    var jamKerja by remember { mutableStateOf("") }
