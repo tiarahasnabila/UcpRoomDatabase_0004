76f91df104b7cee9d52dc015a90ab72eb505bb84
tiarahasnabila <tiarahasnabila@gmail.com>
2024-12-24 13:10:20 +0700

    menambahkan file myapp class main untuk mengelola data

---
+++ app/src/main/java/com/example/ucp2pam/ViewModel/DokterViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2pam.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.database.Dokter
+import com.example.ucp2pam.database.Jadwal
+import com.example.ucp2pam.database.JadwalDatabase
+import kotlinx.coroutines.launch
+
+class MainViewModel (application: Application) : AndroidViewModel(application){
+    private val database = JadwalDatabase.getDatabase(application)
+    private val dokterDao = database.dokterDao()
+    private val jadwalDao = database.jadwalDao()
